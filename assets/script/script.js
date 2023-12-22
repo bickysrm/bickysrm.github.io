fc4b43442733c6c9766a849170d2019d79a6f2ae
bickysharma519 <sharmabicky519@gmail.com>
2023-12-22 12:25:59 +0530

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+const text = document.querySelector(".sec-text");
+
+const Load = () => {
+    setTimeout(() => {
+        text.textContent = "Web Developer";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Youtuber";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Freelancer";
+    }, 8000); //1s = 1000 milliseconds
+}
+
+Load();
+setInterval(Load, 12000);
